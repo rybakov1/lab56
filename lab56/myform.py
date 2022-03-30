f9c0ee2aa84b88ba3f0d772d36cb30d7f7123dfd
Владислав <rybvlad4@gmail.com>
2022-03-30 15:14:04 +0300

    update myform.py

--- lab56/myform.py
+++ lab56/myform.py
@@ -3,13 +3,24 @@ import re
 
 @post('/meow', method='post')
 def my_form():
+    print(check_mob())
+
     mail = request.forms.get('ADDRESS')
     question = request.forms.get('QUESTION')
 
     pattern = r'(\w+)@([A-Z0-9]+)\.([A-Z]{2,4})'  
-    mem = re.findall(pattern, mail, flags=re.IGNORECASE) 
-    if mem.count != 3:
-        message = "Incorrect email!"
+    correctMail = re.match(pattern, mail, flags=re.IGNORECASE)
+    if (correctMail is None):
+        message = "Email input is incorrect!"
     else:
-        message = "Thanks! The answer will be sent to the mail {}".format(mem)
+        message = "Thanks! The answer will be sent to the mail {}".format(mail)
     return message
+
+def check_mob():
+    mobile_reg = '\+7\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}'
+    mobile = '+7 (911) 123-21-31' 
+    correct_tel = re.match(mobile_reg, mobile)
+    if(correct_tel is None):
+        return "NotFine"
+    else:
+        return "Fine"
